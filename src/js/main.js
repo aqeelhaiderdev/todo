d0d1744b86bd2f67d49ebb635cf6a08b269d7685
Aqeel Haider <aqeelhaiderdev@gmail.com>
2023-05-19 15:21:11 +0500

    local Storage Implimenation

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,15 @@ const listRow = document.querySelector(".list-row");
 const submitButton = document.querySelector(".submit");
 const searchBox = document.querySelector(".search-section");
 const InputText = document.querySelector(".search");
+const saveData = function () {
+  localStorage.setItem("Data", list.innerHTML);
+};
+
+const showData = function () {
+  list.innerHTML = localStorage.getItem("Data");
+};
+
+showData();
 
 submitButton.addEventListener("click", function () {
   const getValue = InputText.value;
@@ -13,13 +22,16 @@ submitButton.addEventListener("click", function () {
     const html = `<li class="list-row">${getValue} <span class="cross">&#10005;</span></li>`;
     list.insertAdjacentHTML("beforeend", html);
     InputText.value = "";
+    saveData();
   }
 });
 
 list.addEventListener("click", function (e) {
   if (e.target.tagName === "LI") {
     e.target.classList.toggle("checked");
+    saveData();
   } else if (e.target.tagName === "SPAN") {
     e.target.parentElement.remove();
+    saveData();
   }
 });
